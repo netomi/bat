1d623deb1773fc4f40402519c61d36c1301b8692
netomi <thomas.neidhart@gmail.com>
2022-07-10 09:16:55 +0200

    Copy internal class to where it is being used.

--- dexfile/src/main/java/com/github/netomi/bat/dexfile/io/DexFileWriter.kt
+++ dexfile/src/main/java/com/github/netomi/bat/dexfile/io/DexFileWriter.kt
@@ -16,7 +16,10 @@
 package com.github.netomi.bat.dexfile.io
 
 import com.github.netomi.bat.dexfile.*
-import com.github.netomi.bat.dexfile.DataItemMapImpl
+import com.github.netomi.bat.dexfile.annotation.*
+import com.github.netomi.bat.dexfile.annotation.Annotation
+import com.github.netomi.bat.dexfile.debug.DebugInfo
+import com.github.netomi.bat.dexfile.visitor.DataItemVisitor
 import com.github.netomi.bat.dexfile.visitor.DexFileVisitor
 import com.google.common.hash.Hashing
 import java.io.OutputStream
@@ -28,6 +31,7 @@ class DexFileWriter(private val outputStream: OutputStream) : DexFileVisitor {
     private var dataOffset = 0
     private var dataSize   = 0
 
+    @Suppress("UnstableApiUsage")
     override fun visitDexFile(dexFile: DexFile) {
         dataItemMap = DataItemMapImpl()
         dataItemMap.collectDataItems(dexFile)
@@ -204,5 +208,135 @@ class DexFileWriter(private val outputStream: OutputStream) : DexFileVisitor {
         } else {
             header.updateLinkData(0, 0)
         }
+    }
+}
+
+private class DataItemMapImpl : DataItem.Map {
+    private val dataItemMap: MutableMap<Int, MutableSet<DataItem>> = mutableMapOf()
+    private val offsetMap:   MutableMap<DataItem, Int>             = mutableMapOf()
+
+    fun collectDataItems(dexFile: DexFile) {
+        dexFile.dataItemsAccept(DataSectionItemCollector(this))
+    }
+
+    fun writeDataItems(mapList: MapList, output: DexDataOutput) {
+        writeDataItems(mapList, output, DexConstants.TYPE_TYPE_LIST)
+        writeDataItems(mapList, output, DexConstants.TYPE_ANNOTATION_SET_REF_LIST)
+        writeDataItems(mapList, output, DexConstants.TYPE_ANNOTATION_SET_ITEM)
+        // write the code items before the class data as it references its offset
+        // using uleb128 encoding which is a variable length encoding. If the code
+        // items were written afterwards, its exact offset would not be known
+        // and thus the size of the class data items may vary.
+        writeDataItems(mapList, output, DexConstants.TYPE_CODE_ITEM)
+        writeDataItems(mapList, output, DexConstants.TYPE_CLASS_DATA_ITEM)
+        writeDataItems(mapList, output, DexConstants.TYPE_STRING_DATA_ITEM)
+        writeDataItems(mapList, output, DexConstants.TYPE_DEBUG_INFO_ITEM)
+        writeDataItems(mapList, output, DexConstants.TYPE_ANNOTATION_ITEM)
+        writeDataItems(mapList, output, DexConstants.TYPE_ENCODED_ARRAY_ITEM)
+        writeDataItems(mapList, output, DexConstants.TYPE_ANNOTATIONS_DIRECTORY_ITEM)
+    }
+
+    fun updateOffsets(dexFile: DexFile) {
+        dexFile.dataItemsAccept(object : DataItemVisitor {
+            override fun visitAnyDataItem(dexFile: DexFile, dataItem: DataItem) {
+                dataItem.updateOffsets(this@DataItemMapImpl)
+            }
+        })
+    }
+
+    fun addDataItem(dataItem: DataItem) {
+        val dataItemSet = getDataItems(dataItem.itemType)
+        dataItemSet.add(dataItem)
+    }
+
+    fun getDataItems(type: Int): MutableSet<DataItem> {
+        val dataItemSet = dataItemMap.getOrDefault(type, LinkedHashSet())
+        dataItemMap.putIfAbsent(type, dataItemSet)
+        return dataItemSet
+    }
+
+    private fun writeDataItems(mapList: MapList, output: DexDataOutput, type: Int) {
+        val dataItemSet: Set<DataItem>? = dataItemMap[type]
+        if (dataItemSet != null && dataItemSet.isNotEmpty()) {
+            val align = dataItemSet.iterator().next().dataAlignment
+            output.writeAlignmentPadding(align)
+            mapList.updateMapItem(type, dataItemSet.size, output.offset)
+            for (dataItem in dataItemSet) {
+                val dataItemOffset = output.offset
+                dataItem.updateOffsets(this)
+                dataItem.write(output)
+                offsetMap[dataItem] = dataItemOffset
+            }
+        }
+    }
+
+    override fun getOffset(item: DataItem?): Int {
+        val offset = offsetMap[item]
+        return offset ?: 0
+    }
+}
+
+private class DataSectionItemCollector(private val dataItemMap: DataItemMapImpl) : DataItemVisitor {
+
+    override fun visitAnyDataItem(dexFile: DexFile, dataItem: DataItem) {}
+
+    override fun visitStringData(dexFile: DexFile, stringID: StringID, stringData: StringData) {
+        dataItemMap.addDataItem(stringData)
+    }
+
+    override fun visitParameterTypes(dexFile: DexFile, protoID: ProtoID, typeList: TypeList) {
+        dataItemMap.addDataItem(typeList)
+    }
+
+    override fun visitClassData(dexFile: DexFile, classDef: ClassDef, classData: ClassData) {
+        dataItemMap.addDataItem(classData)
+    }
+
+    override fun visitInterfaceTypes(dexFile: DexFile, classDef: ClassDef, typeList: TypeList) {
+        dataItemMap.addDataItem(typeList)
+    }
+
+    override fun visitStaticValuesArray(dexFile: DexFile, classDef: ClassDef, encodedArray: EncodedArray) {
+        dataItemMap.addDataItem(encodedArray)
+    }
+
+    override fun visitAnnotationsDirectory(dexFile: DexFile, classDef: ClassDef, annotationsDirectory: AnnotationsDirectory) {
+        dataItemMap.addDataItem(annotationsDirectory)
+    }
+
+    override fun visitClassAnnotations(dexFile: DexFile, annotationsDirectory: AnnotationsDirectory, annotationSet: AnnotationSet) {
+        dataItemMap.addDataItem(annotationSet)
+    }
+
+    override fun visitFieldAnnotations(dexFile: DexFile, fieldAnnotation: FieldAnnotation, annotationSet: AnnotationSet) {
+        dataItemMap.addDataItem(annotationSet)
+    }
+
+    override fun visitMethodAnnotations(dexFile: DexFile, methodAnnotation: MethodAnnotation, annotationSet: AnnotationSet) {
+        dataItemMap.addDataItem(annotationSet)
+    }
+
+    override fun visitParameterAnnotations(dexFile: DexFile, parameterAnnotation: ParameterAnnotation, annotationSetRefList: AnnotationSetRefList) {
+        dataItemMap.addDataItem(annotationSetRefList)
+    }
+
+    override fun visitAnnotationSet(dexFile: DexFile, annotationSetRef: AnnotationSetRef, annotationSet: AnnotationSet) {
+        dataItemMap.addDataItem(annotationSet)
+    }
+
+    override fun visitAnnotation(dexFile: DexFile, annotationSet: AnnotationSet, index: Int, annotation: Annotation) {
+        dataItemMap.addDataItem(annotation)
+    }
+
+    override fun visitCode(dexFile: DexFile, encodedMethod: EncodedMethod, code: Code) {
+        dataItemMap.addDataItem(code)
+    }
+
+    override fun visitDebugInfo(dexFile: DexFile, code: Code, debugInfo: DebugInfo) {
+        dataItemMap.addDataItem(debugInfo)
+    }
+
+    override fun visitCallSite(dexFile: DexFile, callSiteID: CallSiteID, callSite: CallSite) {
+        dataItemMap.addDataItem(callSite)
     }
 }
