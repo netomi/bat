532c29d832462179bdb15756a797646caf2b1f8e
netomi <thomas.neidhart@gmail.com>
2022-07-20 10:52:36 +0200

    Add stringmatcher tests.

---
+++ core/src/test/java/com/github/netomi/bat/util/StringMatcherTest.kt
@@ -0,0 +1,91 @@
+/*
+ *  Copyright (c) 2020-2022 Thomas Neidhart.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.github.netomi.bat.util
+
+import kotlin.test.Test
+import kotlin.test.assertFailsWith
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+abstract class StringMatcherTest {
+    abstract fun matcher(regularExpression: String): StringMatcher
+}
+
+class ClassNameMatcherTest : StringMatcherTest() {
+
+    override fun matcher(regularExpression: String): StringMatcher {
+        return classNameMatcher(regularExpression)
+    }
+
+    @Test
+    fun noWildCard() {
+        val matcher = matcher("abc")
+
+        assertTrue(matcher.matches("abc"))
+        assertFalse(matcher.matches(""))
+        assertFalse(matcher.matches("abcde"))
+        assertFalse(matcher.matches("123abc"))
+    }
+
+    @Test
+    fun singleStar() {
+        val matcher = matcher("abc/def*")
+
+        assertTrue(matcher.matches("abc/def"))
+        assertFalse(matcher.matches(""))
+        assertTrue(matcher.matches("abc/defgh"))
+        assertFalse(matcher.matches("123abc/def"))
+        assertFalse(matcher.matches("abc/def/ghi"))
+    }
+
+    @Test
+    fun doubleStar() {
+        val matcher = matcher("abc/def**")
+
+        assertTrue(matcher.matches("abc/def"))
+        assertFalse(matcher.matches(""))
+        assertTrue(matcher.matches("abc/defgh"))
+        assertFalse(matcher.matches("123abc/def"))
+        assertTrue(matcher.matches("abc/def/ghi"))
+    }
+
+    @Test
+    fun questionMark() {
+        val matcher = matcher("abc/def?")
+
+        assertFalse(matcher.matches("abc/def"))
+        assertFalse(matcher.matches(""))
+        assertTrue(matcher.matches("abc/defg"))
+        assertFalse(matcher.matches("abc/defgh"))
+        assertFalse(matcher.matches("123abc/def"))
+        assertFalse(matcher.matches("abc/def/"))
+    }
+
+    @Test
+    fun exclamationMark() {
+        val matcher = matcher("!abc/def*")
+
+        assertFalse(matcher.matches("abc/def"))
+        assertTrue(matcher.matches(""))
+        assertFalse(matcher.matches("abc/defg"))
+        assertFalse(matcher.matches("abc/defgh"))
+        assertTrue(matcher.matches("123abc/def"))
+        assertTrue(matcher.matches("abc/def/ghi"))
+
+        assertFailsWith(RuntimeException::class) { matcher("abc/def!/ghi") }
+    }
+}
