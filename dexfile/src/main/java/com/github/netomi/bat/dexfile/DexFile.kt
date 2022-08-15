4e97261d4fcd95431b0020efc9e5b9d24c743936
netomi <thomas.neidhart@gmail.com>
2022-08-15 09:40:07 +0200

    Use accept methods.

--- dexfile/src/main/java/com/github/netomi/bat/dexfile/DexFile.kt
+++ dexfile/src/main/java/com/github/netomi/bat/dexfile/DexFile.kt
@@ -321,11 +321,11 @@ class DexFile private constructor(private var dexFormatInternal: DexFormat? = De
     }
 
     fun classDefsAccept(visitor: ClassDefVisitor) {
-        classDefs.forEach { classDef -> visitor.visitClassDef(this, classDef) }
+        classDefs.forEach { classDef -> classDef.accept(this, visitor) }
     }
 
     fun classDefsAcceptIndexed(visitor: ClassDefVisitorIndexed) {
-        classDefs.forEachIndexed { index, classDef -> visitor.visitClassDef(this, index, classDef) }
+        classDefs.forEachIndexed { index, classDef -> classDef.accept(this, index, visitor) }
     }
 
     fun parallelClassDefsAccept(coroutineContext: CoroutineContext = Dispatchers.Default, visitorSupplier: () -> ClassDefVisitor) {
