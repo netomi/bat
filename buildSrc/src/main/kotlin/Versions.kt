312c89820c7d8852cb849961d2da2512cf29697d
netomi <thomas.neidhart@gmail.com>
2022-09-09 16:23:40 +0200

    Update to kotlin 1.7.10.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,5 +1,5 @@
 object Versions {
-    const val kotlinVersion     = "1.6.21"
+    const val kotlinVersion     = "1.7.10"
     const val coroutinesVersion = "1.6.4"
     const val picocliVersion    = "4.6.3"
     const val antlrVersion      = "4.10.1"
