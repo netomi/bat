326fd726b00ba95f9379288aafcab7b515d1b8e9
netomi <thomas.neidhart@gmail.com>
2022-09-12 08:26:01 +0200

    Simplify EncodedValueVisitor.

--- dexfile/src/main/java/com/github/netomi/bat/dexfile/value/visitor/EncodedValueVisitor.kt
+++ dexfile/src/main/java/com/github/netomi/bat/dexfile/value/visitor/EncodedValueVisitor.kt
@@ -31,7 +31,11 @@ fun filterValuesByType(acceptedType: EncodedValueType, visitor: EncodedValueVisi
 }
 
 fun filterValuesByType(acceptedTypes: EnumSet<EncodedValueType>, visitor: EncodedValueVisitor): EncodedValueVisitor {
-    return EncodedValueFilter(acceptedTypes, visitor)
+    return EncodedValueVisitor { dexFile, value ->
+        if (acceptedTypes.contains(value.valueType)) {
+            value.accept(dexFile, visitor)
+        }
+    }
 }
 
 fun valueCollector(): EncodedValueCollector {
@@ -138,127 +142,6 @@ class EncodedValueCollector: AbstractCollector<EncodedValue>(), EncodedValueVisi
     }
 }
 
-private class EncodedValueFilter(private val acceptedTypes: EnumSet<EncodedValueType>, private val visitor: EncodedValueVisitor) : EncodedValueVisitor {
-
-    override fun visitAnyValue(dexFile: DexFile, value: EncodedValue) {
-        if (accepted(value)) {
-            visitor.visitAnyValue(dexFile, value)
-        }
-    }
-
-    override fun visitAnnotationValue(dexFile: DexFile, value: EncodedAnnotationValue) {
-        if (accepted(value)) {
-            visitor.visitAnnotationValue(dexFile, value)
-        }
-    }
-
-    override fun visitArrayValue(dexFile: DexFile, value: EncodedArrayValue) {
-        if (accepted(value)) {
-            visitor.visitArrayValue(dexFile, value)
-        }
-    }
-
-    override fun visitBooleanValue(dexFile: DexFile, value: EncodedBooleanValue) {
-        if (accepted(value)) {
-            visitor.visitBooleanValue(dexFile, value)
-        }
-    }
-
-    override fun visitByteValue(dexFile: DexFile, value: EncodedByteValue) {
-        if (accepted(value)) {
-            visitor.visitByteValue(dexFile, value)
-        }
-    }
-
-    override fun visitCharValue(dexFile: DexFile, value: EncodedCharValue) {
-        if (accepted(value)) {
-            visitor.visitCharValue(dexFile, value)
-        }
-    }
-
-    override fun visitDoubleValue(dexFile: DexFile, value: EncodedDoubleValue) {
-        if (accepted(value)) {
-            visitor.visitDoubleValue(dexFile, value)
-        }
-    }
-
-    override fun visitEnumValue(dexFile: DexFile, value: EncodedEnumValue) {
-        if (accepted(value)) {
-            visitor.visitEnumValue(dexFile, value)
-        }
-    }
-
-    override fun visitFieldValue(dexFile: DexFile, value: EncodedFieldValue) {
-        if (accepted(value)) {
-            visitor.visitFieldValue(dexFile, value)
-        }
-    }
-
-    override fun visitFloatValue(dexFile: DexFile, value: EncodedFloatValue) {
-        if (accepted(value)) {
-            visitor.visitFloatValue(dexFile, value)
-        }
-    }
-
-    override fun visitIntValue(dexFile: DexFile, value: EncodedIntValue) {
-        if (accepted(value)) {
-            visitor.visitIntValue(dexFile, value)
-        }
-    }
-
-    override fun visitLongValue(dexFile: DexFile, value: EncodedLongValue) {
-        if (accepted(value)) {
-            visitor.visitLongValue(dexFile, value)
-        }
-    }
-
-    override fun visitMethodHandleValue(dexFile: DexFile, value: EncodedMethodHandleValue) {
-        if (accepted(value)) {
-            visitor.visitMethodHandleValue(dexFile, value)
-        }
-    }
-
-    override fun visitMethodTypeValue(dexFile: DexFile, value: EncodedMethodTypeValue) {
-        if (accepted(value)) {
-            visitor.visitMethodTypeValue(dexFile, value)
-        }
-    }
-
-    override fun visitMethodValue(dexFile: DexFile, value: EncodedMethodValue) {
-        if (accepted(value)) {
-            visitor.visitMethodValue(dexFile, value)
-        }
-    }
-
-    override fun visitNullValue(dexFile: DexFile, value: EncodedNullValue) {
-        if (accepted(value)) {
-            visitor.visitNullValue(dexFile, value)
-        }
-    }
-
-    override fun visitShortValue(dexFile: DexFile, value: EncodedShortValue) {
-        if (accepted(value)) {
-            visitor.visitShortValue(dexFile, value)
-        }
-    }
-
-    override fun visitStringValue(dexFile: DexFile, value: EncodedStringValue) {
-        if (accepted(value)) {
-            visitor.visitStringValue(dexFile, value)
-        }
-    }
-
-    override fun visitTypeValue(dexFile: DexFile, value: EncodedTypeValue) {
-        if (accepted(value)) {
-            visitor.visitTypeValue(dexFile, value)
-        }
-    }
-
-    private fun accepted(value: EncodedValue): Boolean {
-        return acceptedTypes.contains(value.valueType)
-    }
-}
-
 private class Multi constructor(       visitor:       EncodedValueVisitor,
                                 vararg otherVisitors: EncodedValueVisitor)
 
@@ -266,115 +149,7 @@ private class Multi constructor(       visitor:       EncodedValueVisitor,
 
     override fun visitAnyValue(dexFile: DexFile, value: EncodedValue) {
         for (visitor in visitors) {
-            visitor.visitAnyValue(dexFile, value)
-        }
-    }
-
-    override fun visitAnnotationValue(dexFile: DexFile, value: EncodedAnnotationValue) {
-        for (visitor in visitors) {
-            visitor.visitAnnotationValue(dexFile, value)
-        }
-    }
-
-    override fun visitArrayValue(dexFile: DexFile, value: EncodedArrayValue) {
-        for (visitor in visitors) {
-            visitor.visitArrayValue(dexFile, value)
-        }
-    }
-
-    override fun visitBooleanValue(dexFile: DexFile, value: EncodedBooleanValue) {
-        for (visitor in visitors) {
-            visitor.visitBooleanValue(dexFile, value)
-        }
-    }
-
-    override fun visitByteValue(dexFile: DexFile, value: EncodedByteValue) {
-        for (visitor in visitors) {
-            visitor.visitByteValue(dexFile, value)
-        }
-    }
-
-    override fun visitCharValue(dexFile: DexFile, value: EncodedCharValue) {
-        for (visitor in visitors) {
-            visitor.visitCharValue(dexFile, value)
-        }
-    }
-
-    override fun visitDoubleValue(dexFile: DexFile, value: EncodedDoubleValue) {
-        for (visitor in visitors) {
-            visitor.visitDoubleValue(dexFile, value)
-        }
-    }
-
-    override fun visitEnumValue(dexFile: DexFile, value: EncodedEnumValue) {
-        for (visitor in visitors) {
-            visitor.visitEnumValue(dexFile, value)
-        }
-    }
-
-    override fun visitFieldValue(dexFile: DexFile, value: EncodedFieldValue) {
-        for (visitor in visitors) {
-            visitor.visitFieldValue(dexFile, value)
-        }
-    }
-
-    override fun visitFloatValue(dexFile: DexFile, value: EncodedFloatValue) {
-        for (visitor in visitors) {
-            visitor.visitFloatValue(dexFile, value)
-        }
-    }
-
-    override fun visitIntValue(dexFile: DexFile, value: EncodedIntValue) {
-        for (visitor in visitors) {
-            visitor.visitIntValue(dexFile, value)
-        }
-    }
-
-    override fun visitLongValue(dexFile: DexFile, value: EncodedLongValue) {
-        for (visitor in visitors) {
-            visitor.visitLongValue(dexFile, value)
-        }
-    }
-
-    override fun visitMethodHandleValue(dexFile: DexFile, value: EncodedMethodHandleValue) {
-        for (visitor in visitors) {
-            visitor.visitMethodHandleValue(dexFile, value)
-        }
-    }
-
-    override fun visitMethodTypeValue(dexFile: DexFile, value: EncodedMethodTypeValue) {
-        for (visitor in visitors) {
-            visitor.visitMethodTypeValue(dexFile, value)
-        }
-    }
-
-    override fun visitMethodValue(dexFile: DexFile, value: EncodedMethodValue) {
-        for (visitor in visitors) {
-            visitor.visitMethodValue(dexFile, value)
-        }
-    }
-
-    override fun visitNullValue(dexFile: DexFile, value: EncodedNullValue) {
-        for (visitor in visitors) {
-            visitor.visitNullValue(dexFile, value)
-        }
-    }
-
-    override fun visitShortValue(dexFile: DexFile, value: EncodedShortValue) {
-        for (visitor in visitors) {
-            visitor.visitShortValue(dexFile, value)
-        }
-    }
-
-    override fun visitStringValue(dexFile: DexFile, value: EncodedStringValue) {
-        for (visitor in visitors) {
-            visitor.visitStringValue(dexFile, value)
-        }
-    }
-
-    override fun visitTypeValue(dexFile: DexFile, value: EncodedTypeValue) {
-        for (visitor in visitors) {
-            visitor.visitTypeValue(dexFile, value)
+            value.accept(dexFile, visitor)
         }
     }
 }
