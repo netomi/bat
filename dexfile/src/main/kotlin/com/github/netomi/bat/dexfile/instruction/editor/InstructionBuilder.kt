3000e93774ae9f32d02c441cf2162130d692a4f5
James Hamilton <mrjameshamilton@gmail.com>
2022-09-17 20:11:25 +0200

    Add new instruction functions to `InstructionBuilder`

--- dexfile/src/main/kotlin/com/github/netomi/bat/dexfile/instruction/editor/InstructionBuilder.kt
+++ dexfile/src/main/kotlin/com/github/netomi/bat/dexfile/instruction/editor/InstructionBuilder.kt
@@ -41,6 +41,14 @@ class InstructionBuilder private constructor(private val dexEditor: DexEditor) {
         return add(LiteralInstruction.of(DexOpCode.CONST, value, destinationRegister))
     }
 
+    fun const4(value: Long, destinationRegister: Int): LiteralInstruction {
+        return add(LiteralInstruction.of(DexOpCode.CONST_4, value, destinationRegister))
+    }
+
+    fun const16(value: Long, destinationRegister: Int): LiteralInstruction {
+        return add(LiteralInstruction.of(DexOpCode.CONST_16, value, destinationRegister))
+    }
+
     fun newArray(type: String, destinationRegister: Int, sizeRegister: Int): TypeInstruction {
         return newArray(dexEditor.addOrGetTypeIDIndex(type), destinationRegister, sizeRegister)
     }
@@ -53,8 +61,20 @@ class InstructionBuilder private constructor(private val dexEditor: DexEditor) {
         return add(FillArrayDataInstruction.of(payload, register))
     }
 
+    fun arrayGetByte(destinationRegister: Int, arrayIndex: Int, index: Int): ArrayInstruction {
+        return add(ArrayInstruction.of(DexOpCode.AGET_BYTE, destinationRegister, arrayIndex, index))
+    }
+
+    fun arrayPutByte(destinationRegister: Int, arrayIndex: Int, index: Int): ArrayInstruction {
+        return add(ArrayInstruction.of(DexOpCode.APUT_BYTE, destinationRegister, arrayIndex, index))
+    }
+
     fun returnVoid(): ReturnInstruction {
         return add(ReturnInstruction.of(DexOpCode.RETURN_VOID))
+    }
+
+    fun `return`(register: Int): ReturnInstruction {
+        return add(ReturnInstruction.of(DexOpCode.RETURN, register))
     }
 
     fun invokeDirect(methodIndex: Int, vararg registers: Int): MethodInstruction {
@@ -63,6 +83,54 @@ class InstructionBuilder private constructor(private val dexEditor: DexEditor) {
 
     fun invokeDirect(classType: String, name: String, parameterTypes: List<String>, returnType: String, vararg registers: Int): MethodInstruction {
         return invokeDirect(dexEditor.addOrGetMethodIDIndex(classType, name, parameterTypes, returnType), *registers)
+    }
+
+    fun invokeVirtual(methodIndex: Int, vararg registers: Int): MethodInstruction {
+        return add(MethodInstruction.of(DexOpCode.INVOKE_VIRTUAL, methodIndex, *registers))
+    }
+
+    fun invokeVirtual(classType: String, name: String, parameterTypes: List<String>, returnType: String, vararg registers: Int): MethodInstruction {
+        return invokeVirtual(dexEditor.addOrGetMethodIDIndex(classType, name, parameterTypes, returnType), *registers)
+    }
+
+    fun invokeStatic(methodIndex: Int, vararg registers: Int): MethodInstruction {
+        return add(MethodInstruction.of(DexOpCode.INVOKE_STATIC, methodIndex, *registers))
+    }
+
+    fun invokeStatic(classType: String, name: String, parameterTypes: List<String>, returnType: String, vararg registers: Int): MethodInstruction {
+        return invokeStatic(dexEditor.addOrGetMethodIDIndex(classType, name, parameterTypes, returnType), *registers)
+    }
+
+    fun staticGetObject(classType: String, name: String, type: String, register: Int): FieldInstruction {
+        return add(FieldInstruction.of(DexOpCode.SGET_OBJECT, dexEditor.addOrGetFieldIDIndex(classType, name, type), register))
+    }
+
+    fun intToChar(registerA: Int, registerB: Int): ConversionInstruction {
+        return add(ConversionInstruction.of(DexOpCode.INT_TO_CHAR, registerA, registerB))
+    }
+
+    fun intToByte(registerA: Int, registerB: Int): ConversionInstruction {
+        return add(ConversionInstruction.of(DexOpCode.INT_TO_BYTE, registerA, registerB))
+    }
+
+    fun moveResult(register: Int): MoveInstruction {
+        return add(MoveInstruction.of(DexOpCode.MOVE_RESULT, register))
+    }
+
+    fun addIntLit8(literal: Int, registerA: Int, registerB: Int): ArithmeticLiteralInstruction {
+        return add(ArithmeticLiteralInstruction.of(DexOpCode.ADD_INT_LIT8, literal, registerA, registerB))
+    }
+
+    fun ifEqualZero(register: Int, label: String): BranchInstruction {
+        return add(BranchInstruction.of(DexOpCode.IF_EQZ, label, register))
+    }
+
+    fun goto(label: String): BranchInstruction {
+        return add(BranchInstruction.of(DexOpCode.GOTO, label))
+    }
+
+    fun goto16(label: String): BranchInstruction {
+        return add(BranchInstruction.of(DexOpCode.GOTO_16, label))
     }
 
     fun getInstructionSequence(): List<DexInstruction> {
