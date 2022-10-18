fa5351598ca5dcf6dd0814a1961fa4a72f23b3c3
netomi <thomas.neidhart@gmail.com>
2022-10-18 22:18:31 +0200

    Add local variable info printing.

--- jasm/src/main/kotlin/com/github/netomi/bat/jasm/disassemble/CodePrinter.kt
+++ jasm/src/main/kotlin/com/github/netomi/bat/jasm/disassemble/CodePrinter.kt
@@ -18,9 +18,7 @@ package com.github.netomi.bat.jasm.disassemble
 
 import com.github.netomi.bat.classfile.ClassFile
 import com.github.netomi.bat.classfile.Method
-import com.github.netomi.bat.classfile.attribute.Attribute
-import com.github.netomi.bat.classfile.attribute.CodeAttribute
-import com.github.netomi.bat.classfile.attribute.LineNumberTableAttribute
+import com.github.netomi.bat.classfile.attribute.*
 import com.github.netomi.bat.classfile.attribute.visitor.CodeAttributeVisitor
 import com.github.netomi.bat.classfile.attribute.visitor.MethodAttributeVisitor
 import com.github.netomi.bat.io.IndentingPrinter
@@ -29,17 +27,25 @@ internal class CodePrinter constructor(private val printer:         IndentingPri
                                        private val constantPrinter: ConstantPrinter): MethodAttributeVisitor, CodeAttributeVisitor {
 
     private var debugState: MutableMap<Int, MutableList<String>> = mutableMapOf()
+    private var localVariableInfos: MutableMap<Int, MutableList<LocalVariableInfo>> = mutableMapOf()
 
     override fun visitAnyAttribute(classFile: ClassFile, attribute: Attribute) {}
 
     override fun visitCode(classFile: ClassFile, method: Method, attribute: CodeAttribute) {
         debugState.clear()
+        localVariableInfos.clear()
 
         printer.println(".stack ${attribute.maxStack}")
         printer.println(".locals ${attribute.maxLocals}")
 
         // fill debug info
         attribute.attributesAccept(classFile, method, this)
+        for ((_, infos) in localVariableInfos) {
+            for (localVariableInfo in infos) {
+                handleStartLocal(localVariableInfo)
+                handleEndLocal(localVariableInfo)
+            }
+        }
 
         // collect branch target / label infos.
         val branchTargetPrinter = BranchTargetPrinter(printer)
@@ -50,6 +56,81 @@ internal class CodePrinter constructor(private val printer:         IndentingPri
     }
 
     // CodeAttributeVisitor.
+
+    override fun visitLocalVariableTable(classFile: ClassFile, method: Method, code: CodeAttribute, attribute: LocalVariableTableAttribute) {
+        for (entry in attribute) {
+            addLocalVariable(entry.startPC, entry.length, entry.variableIndex, entry.getName(classFile), entry.getDescriptor(classFile), null)
+        }
+    }
+
+    override fun visitLocalVariableTypeTable(classFile: ClassFile, method: Method, code: CodeAttribute, attribute: LocalVariableTypeTableAttribute) {
+        for (entry in attribute) {
+            addLocalVariable(entry.startPC, entry.length, entry.variableIndex, entry.getName(classFile), null, entry.getSignature(classFile))
+        }
+    }
+
+    private fun addLocalVariable(startPC: Int, length: Int, registerNum: Int, name: String, descriptor: String?, signature: String?) {
+        val infos = localVariableInfos.computeIfAbsent(startPC) { ArrayList() }
+
+        var localVariableInfo = infos.firstOrNull { it.startPC == startPC && it.name == name }
+        if (localVariableInfo == null) {
+            localVariableInfo = LocalVariableInfo(startPC, length, registerNum, name)
+            infos.add(localVariableInfo)
+        }
+
+        localVariableInfo.updateDescriptor(descriptor)
+        localVariableInfo.updateSignature(signature)
+    }
+
+    private fun handleStartLocal(localVariableInfo: LocalVariableInfo) {
+        val info = buildString {
+            append(".local ")
+            append(localVariableInfo.variableIndex)
+            append(", ")
+
+            append("\"")
+            append(localVariableInfo.name)
+            append("\"")
+
+            if (localVariableInfo.descriptor != null) {
+                append(":")
+                append(localVariableInfo.descriptor)
+            }
+
+            if (localVariableInfo.signature != null) {
+                append(", \"")
+                append(localVariableInfo.signature)
+                append("\"")
+            }
+        }
+
+        addDebugInfo(localVariableInfo.startPC, info)
+    }
+
+    private fun handleEndLocal(localVariableInfo: LocalVariableInfo) {
+        val info = buildString {
+            append(".end local ")
+            append(localVariableInfo.variableIndex)
+            append(" # ")
+
+            append("\"")
+            append(localVariableInfo.name)
+            append("\"")
+
+            if (localVariableInfo.descriptor != null) {
+                append(":")
+                append(localVariableInfo.descriptor)
+            }
+
+            if (localVariableInfo.signature != null) {
+                append(", \"")
+                append(localVariableInfo.signature)
+                append("\"")
+            }
+        }
+
+        addDebugInfo(localVariableInfo.startPC + localVariableInfo.length, info)
+    }
 
     override fun visitLineNumberTable(classFile: ClassFile, method: Method, code: CodeAttribute, attribute: LineNumberTableAttribute) {
         for (entry in attribute) {
@@ -64,5 +145,26 @@ internal class CodePrinter constructor(private val printer:         IndentingPri
     private fun addDebugInfo(offset: Int, info: String) {
         val infos = debugState.computeIfAbsent(offset) { ArrayList() }
         infos.add(info)
+    }
+}
+
+internal data class LocalVariableInfo
+    constructor(val startPC:       Int,
+                val length:        Int,
+                val variableIndex: Int,
+                val name:          String,
+                var descriptor:    String? = null,
+                var signature:     String? = null) {
+
+    fun updateDescriptor(descriptor: String?) {
+        if (descriptor != null) {
+            this.descriptor = descriptor
+        }
+    }
+
+    fun updateSignature(signature: String?) {
+        if (signature != null) {
+            this.signature = signature
+        }
     }
 }
